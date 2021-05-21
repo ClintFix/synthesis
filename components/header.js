6109bbe019cc1838eed4c38232f2da77f46c5c03
ClintFix <clint.fix@gmail.com>
2021-05-21 16:01:08 -0600

    working on css

--- components/header.js
+++ components/header.js
@@ -5,7 +5,6 @@ export default function Navigation() {
         <div className={styles.nav}>
             <div className={styles.container}>
                 <h1>Clint Fix</h1>
-                <h1>Test Block</h1>
             </div>
         </div>
     );
