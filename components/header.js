6ab2f078dd4b3bfdc987e5f1d5903eec204f85f2
ClintFix <clint.fix@gmail.com>
2021-05-21 13:49:02 -0600

    first commit

---
+++ components/header.js
@@ -0,0 +1,9 @@
+import styles from './header.module.css';
+
+export default function Navigation() {
+    return (
+        <div className={styles.container}>
+            <h1>Clint Fix</h1>
+        </div>
+    );
+}
