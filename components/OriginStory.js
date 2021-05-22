e5f2aeaebd8256dba5039925525fec7a1773bee3
ClintFix <clint.fix@gmail.com>
2021-05-22 17:21:03 -0600

    added origin story

--- components/OriginStory.js
+++ components/OriginStory.js
@@ -6,13 +6,48 @@ export default function OriginStory() {
             <div className={styles.originContainer}>
                 <h1 className={styles.headingOne}>Origin Story</h1>
                 <p className={styles.originText}>
-                    Six years ago, Elon Musk asked me to start an experimental school with him at SpaceX.
+                    Match quality, the term economists use for the degree of fit between and individual&apos;s abilities and interests and the work they do, is something I&apos;ve been hunting for since high school. The search hasn&apos;t been without its twists, turns, and upsets, but without the journey I wouldn&apos;t be where I am today: with a strong match *and* breadth of experience and knowledge. 
                     <br />
                     <br />
-                    The goal was to develop students who are enthralled by complexity and solving for the unknown.
+                    Though my career began as a Mechanical Engineer and I had the ability to succeed in the field, a recurring pattern emerged in my early career: I kept getting distracted by software and various problems that could be solved. My interests obviously laid outside of mechanical engineering. 
                     <br />
                     <br />
-                    Synthesis is the most innovative learning experience from that school. It is designed to cultivate student voice, strategic thinking, and collaborative problem solving.
+                    In late 2020, sitting in my home office feeling a bit too sorry for myself as I scrolled Twitter, I came across a tweet by Austen Allred, the founder of Lambda School. I had been following Lambda for some time — both because I love the startup world and follow many YC companies and because I love the alternative education space. 
+                    <br />
+                    <br />
+                    There it was: another tweet showing off the results of a Lambda grad. But this time the tweet hit me hard. The local small business I had started a couple years prior to keep food on the table after parting ways with a company I had co-founded was hurting...bad. The shutdowns all but decimated it. 
+                    <br />
+                    <br />
+                    I couldn&apos;t stand the thought of going back to mechanical engineering. I wasn&apos;t in a spot to start another company. And the tinge of regret for having not learned software development was growing with every passing year. 
+                    <br />
+                    <br />
+                    So I lept. I dove into Lambda School&apos;s full-stack web development program in January of 2021. In the last 5 months I&apos;ve learned Javascript, React, Redux, Node/Express, SQL, PostgeSQL, and Python. 
+                    <br />
+                    <br />
+                    I was hungry for more so I started a side-project around one of the many ideas I've come up with over the years: PodChat. It&apos;s not live yet, but you can follow the progress at [http://PodChatApp.com](http://podchatapp.com) and view the project on GitHub: INSERT GITHUB LINK. 
+                    <br />
+                    <br />
+                    Looking forward, I have one primary goal: becoming a valuable engineering team member with Synthesis. I built this site using Next.js to learn the framework and show that I have the abilities to succeed. But it goes beyond ability. It takes match quality, which means it has to go beyond mere skills. It has to match interests, especially in a fast-moving start-up environment. 
+                    <br />
+                    <br />
+                    Since long before I had kids, I envisioned taking them through a different educational system — one that would teach them how to think rather than what to think. 
+                    <br />
+                    One that would inspire them to grow and learn. 
+                    <br />
+                    One that fostered their creativity. 
+                    <br />
+                    <br />
+                    Synthesis would be far more than just a job at a hot startup. It&apos;s building something important that I&apos;ve cared about and have been seeking for my own family. 
+                    <br />
+                    <br />
+                    I can&apos;t wait to contribute to the mission.
+                    <br />
+                    <br />
+                    <br />
+                    Cheers!
+                    <br />
+                    <br />
+                    Clint Fix, A guy that can&apos;t wait to have a title next to his name at Synthesis
                 </p>
             </div>
         </div>
