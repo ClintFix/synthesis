b0b596cd985f94390cb92adc9f67d04df5d3e99d
ClintFix <clint.fix@gmail.com>
2021-05-22 12:37:40 -0600

    got text displaying over video

--- components/BackgroundVideo.js
+++ components/BackgroundVideo.js
@@ -2,10 +2,8 @@ import styles from './backgroundVideo.module.css';
 
 export default function BackgroundVideo() {
     return (
-        <div className={styles.backgroundVideo}>
-            <video autoPlay muted loop>
-                <source src="https://assets-global.website-files.com/5fd068558c772c0292e5f529/5fd15c96f42a2947b53c3d4d_stringtheory_loop-transcode.mp4" type="video/mp4"/>
-            </video>
-        </div>
+        <video autoPlay muted loop style={{backgroundImage: 'url("https://assets-global.website-files.com/5fd068558c772c0292e5f529/5fd15c96f42a2947b53c3d4d_stringtheory_loop-poster-00001.jpg")'}}>
+            <source src="https://assets-global.website-files.com/5fd068558c772c0292e5f529/5fd15c96f42a2947b53c3d4d_stringtheory_loop-transcode.mp4" type="video/mp4"/>
+        </video>
     );
 }
--- components/hero.js
+++ components/hero.js
@@ -5,6 +5,9 @@ export default function Hero() {
     return (
         <div className={styles.hero}>
             <BackgroundVideo />
+            <div className={styles.heroText}>
+                <h1>Where kids become problem solvers</h1>
+            </div>
         </div>
     );
 }
