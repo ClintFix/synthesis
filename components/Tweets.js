f4b9b5b52fd9b41fc6e413dc38e638d5e1aae2d4
ClintFix <clint.fix@gmail.com>
2021-05-22 20:54:02 -0600

    tweets rendering

--- components/Tweets.js
+++ components/Tweets.js
@@ -2,15 +2,26 @@ import { TwitterTweetEmbed } from 'react-twitter-embed';
 import styles from './tweets.module.css';
 
 export default function Tweets({tweets}) {
-    console.log(tweets.data);
+    const filteredTweets = tweets.data.filter(tweet => {
+        if (!tweet.in_reply_to_user_id) {
+            return tweet;
+        }
+    });
+    console.log(filteredTweets);
     return (
         <div className={styles.tweets}>
             <div className={styles.tweetsContainer}>
                 <h1 className={styles.headingOne}>Latest @Synthesis Tweets</h1>
                 <h4 className={styles.headingFour}>( Rendered with getServerSideProps )</h4>
-                <TwitterTweetEmbed
-                    tweetId={'933354946111705097'}
-                />
+                <div className={styles.tweetFlex}>
+                    {
+                        filteredTweets.map(tweet => {
+                            return <TwitterTweetEmbed key={tweet.id} tweetId={tweet.id} options={{
+                                width: '310' 
+                              }}/>;
+                        })
+                    }
+                </div>
             </div>
         </div>
 
