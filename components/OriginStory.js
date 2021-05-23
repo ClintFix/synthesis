d1554ec9d0e720e13057187ecd3a64b162d18dcb
ClintFix <clint.fix@gmail.com>
2021-05-23 16:04:56 -0600

    fixed more grammar

--- components/OriginStory.js
+++ components/OriginStory.js
@@ -6,7 +6,7 @@ export default function OriginStory() {
             <div className={styles.originContainer}>
                 <h1 className={styles.headingOne}>Origin Story</h1>
                 <p className={styles.originText}>
-                    Match quality, the term economists use for the degree of fit between an individual&apos;s abilities and interests and the work they do, is something I&apos;ve been hunting for since high school. The search hasn&apos;t been without its twists, turns, and upsets, but without the journey I wouldn&apos;t be where I am today: with breadth of experience and finally a career that feels like a perfect match.
+                    Match quality, the term economists use for the degree of fit between an individual&apos;s abilities and interests and the work they do, is something for which I&apos;ve been searching since high school. The search hasn&apos;t been without its twists, turns, and upsets, but without the journey I wouldn&apos;t be where I am today: with breadth of experience and finally a career that feels like a perfect match.
                     <br />
                     <br />
                     Though my career began as a Mechanical Engineer, a recurring pattern emerged in my early career: I kept getting distracted by various problems that could be solved with software. My interests obviously laid outside of mechanical engineering. 
@@ -15,13 +15,13 @@ export default function OriginStory() {
                     In late 2020, while sitting in my home office feeling a bit too sorry for myself as I scrolled Twitter, I came across a tweet by Austen Allred, the founder of Lambda School. I had been following Lambda for some time — both because I love the alternative education space and because I love the startup world and follow many YC companies.
                     <br />
                     <br />
-                    There was nothing particular special with the tweet. It was just another tweet showing off the results of a Lambda grad. But this time the tweet hit me hard. The local small business I had started a couple years prior to keep food on the table after parting ways with a company I had co-founded was hurting...bad. The shutdowns all but decimated it. 
+                    There was nothing particulary special with the tweet. It was just another tweet showing off the results of a Lambda grad. But this time the tweet hit me hard. The local small business I had started a couple years prior to keep food on the table after parting ways with a company I had co-founded was hurting...bad. The shutdowns all but decimated it. 
                     <br />
                     <br />
                     I couldn&apos;t stand the thought of going back into mechanical engineering. I wasn&apos;t in a spot to start another company. And the tinge of regret for having not learned software development was growing with every passing year. 
                     <br />
                     <br />
-                    So I lept. I dove into Lambda School&apos;s full-stack web development program in January of 2021. In the last 5 months I&apos;ve learned Javascript, React, Redux, Node/Express, SQL, PostgeSQL, and Python. 
+                    So I lept. I dove into Lambda School&apos;s full-stack web development program in January of 2021. In the last 5 months I&apos;ve learned Javascript, React, Redux, Node/Express, SQL, PostgreSQL, and Python. 
                     <br />
                     <br />
                     I was hungry for more so I started a side-project around one of the many ideas I&apos;ve come up with over the years: PodChat. It&apos;s not live yet, but you can follow the progress at <a href="http://PodChatApp.com" alt="PodChat" target="_blank" rel="noreferrer">PodChatApp.com</a> and view the project on GitHub: <a href="https://github.com/pod-chat" alt="PodChat Github" target="_blank" rel="noreferrer">PodChat Github</a>.
@@ -30,7 +30,7 @@ export default function OriginStory() {
                     Looking forward, <span style={{fontWeight: '600'}}>I have one primary goal: becoming a valuable engineering team member with Synthesis.</span> I built this site using Next.js to learn the framework and show that I have the ability to learn quickly.
                     <br />
                     <br />
-                    But success goes beyond mere skills and ability. A loyal teammate requires match quality -- a teammate that is also immensely iterested in the company&apos;s mission.
+                    But success goes beyond mere skills and ability. A loyal teammate requires match quality -- a teammate that is also immensely interested in the company&apos;s mission.
                     <br />
                     <br />
                     Since long before I had kids, I envisioned taking them through a different educational system —
