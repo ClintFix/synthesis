f21b3ecc40a34b93ace4fcf1a64f932e70db08d2
ClintFix <clint.fix@gmail.com>
2021-05-23 15:53:41 -0600

    fixed some spelling/grammar in origin story

--- components/OriginStory.js
+++ components/OriginStory.js
@@ -6,31 +6,37 @@ export default function OriginStory() {
             <div className={styles.originContainer}>
                 <h1 className={styles.headingOne}>Origin Story</h1>
                 <p className={styles.originText}>
-                    Match quality, the term economists use for the degree of fit between and individual&apos;s abilities and interests and the work they do, is something I&apos;ve been hunting for since high school. The search hasn&apos;t been without its twists, turns, and upsets, but without the journey I wouldn&apos;t be where I am today: a career path with a strong match *and* breadth of experience and knowledge. 
+                    Match quality, the term economists use for the degree of fit between an individual&apos;s abilities and interests and the work they do, is something I&apos;ve been hunting for since high school. The search hasn&apos;t been without its twists, turns, and upsets, but without the journey I wouldn&apos;t be where I am today: with breadth of experience and finally a career that feels like a perfect match.
                     <br />
                     <br />
-                    Through my career began as a Mechanical Engineer and I had the ability to succeed in the field, a recurring pattern emerged in my early career: I kept getting distracted by software and various problems that could be solved. My interests obviously laid outside of mechanical engineering. 
+                    Though my career began as a Mechanical Engineer, a recurring pattern emerged in my early career: I kept getting distracted by various problems that could be solved with software. My interests obviously laid outside of mechanical engineering. 
                     <br />
                     <br />
-                    In late 2020, sitting in my home office feeling a bit too sorry for myself as I scrolled Twitter, I came across a tweet by Austen Allred, the founder of Lambda School. I had been following Lambda for some time — both because I love the startup world and follow many YC companies and because I love the alternative education space. 
+                    In late 2020, while sitting in my home office feeling a bit too sorry for myself as I scrolled Twitter, I came across a tweet by Austen Allred, the founder of Lambda School. I had been following Lambda for some time — both because I love the alternative education space and because I love the startup world and follow many YC companies.
                     <br />
                     <br />
-                    There it was: another tweet showing off the results of a Lambda grad. But this time the tweet hit me hard. The local small business I had started a couple years prior to keep food on the table after parting ways with a company I had co-founded was hurting...bad. The shutdowns all but decimated it. 
+                    There was nothing particular special with the tweet. It was just another tweet showing off the results of a Lambda grad. But this time the tweet hit me hard. The local small business I had started a couple years prior to keep food on the table after parting ways with a company I had co-founded was hurting...bad. The shutdowns all but decimated it. 
                     <br />
                     <br />
-                    I couldn&apos;t stand the thought of going back to mechanical engineering. I wasn&apos;t in a spot to start another company. And the tinge of regret for having not learned software development was growing with every passing year. 
+                    I couldn&apos;t stand the thought of going back into mechanical engineering. I wasn&apos;t in a spot to start another company. And the tinge of regret for having not learned software development was growing with every passing year. 
                     <br />
                     <br />
                     So I lept. I dove into Lambda School&apos;s full-stack web development program in January of 2021. In the last 5 months I&apos;ve learned Javascript, React, Redux, Node/Express, SQL, PostgeSQL, and Python. 
                     <br />
                     <br />
-                    I was hungry for more so I started a side-project around one of the many ideas I've come up with over the years: PodChat. It&apos;s not live yet, but you can follow the progress at [http://PodChatApp.com](http://podchatapp.com) and view the project on GitHub: INSERT GITHUB LINK. 
+                    I was hungry for more so I started a side-project around one of the many ideas I&apos;ve come up with over the years: PodChat. It&apos;s not live yet, but you can follow the progress at <a href="http://PodChatApp.com" alt="PodChat" target="_blank" rel="noreferrer">PodChatApp.com</a> and view the project on GitHub: <a href="https://github.com/pod-chat" alt="PodChat Github" target="_blank" rel="noreferrer">PodChat Github</a>.
                     <br />
                     <br />
-                    Looking forward, I have one primary goal: becoming a valuable engineering team member with Synthesis. I built this site using Next.js to learn the framework and show that I have the abilities to succeed. But it goes beyond ability. It takes match quality, which means it has to go beyond mere skills. It has to match interests, especially in a fast-moving start-up environment. 
+                    Looking forward, <span style={{fontWeight: '600'}}>I have one primary goal: becoming a valuable engineering team member with Synthesis.</span> I built this site using Next.js to learn the framework and show that I have the ability to learn quickly.
                     <br />
                     <br />
-                    Since long before I had kids, I envisioned taking them through a different educational system — one that would teach them how to think rather than what to think. 
+                    But success goes beyond mere skills and ability. A loyal teammate requires match quality -- a teammate that is also immensely iterested in the company&apos;s mission.
+                    <br />
+                    <br />
+                    Since long before I had kids, I envisioned taking them through a different educational system —
+                    <br />
+                    <br />
+                    One that would teach them <span style={{fontWeight: '600'}}>how</span> to think rather than what to think. 
                     <br />
                     One that would inspire them to grow and learn. 
                     <br />
